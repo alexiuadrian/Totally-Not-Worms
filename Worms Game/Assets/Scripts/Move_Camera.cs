f354c4dda5323b4525eb79ab6806ef0133d7a51d
Alexiu Adrian <48868394+alexiuadrian@users.noreply.github.com>
2020-12-29 13:13:31 +0200

    Minor Cl;eanup

--- Worms Game/Assets/Scripts/Move_Camera.cs
+++ Worms Game/Assets/Scripts/Move_Camera.cs
@@ -15,20 +15,7 @@ public class Move_Camera : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        /*
-        float move_x = 0f;
-        float move_y = 0f;
-        if (Input.GetAxisRaw("Mouse X") > -20 && Input.GetAxisRaw("Mouse X") < 20)
-        {
-            move_x = Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed;
-            if (Input.GetAxisRaw("Mouse Y") > 0)
-            {
-                move_y = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed;
-            }
-        }
-        */
-        
+    {      
         transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed, 0f);
 
     }
