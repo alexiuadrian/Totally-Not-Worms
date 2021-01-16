4fd0c85142ce116bcc2b38a8d4c9cce8a39aa9ab
Alexiu Adrian <48868394+alexiuadrian@users.noreply.github.com>
2021-01-16 12:17:32 +0200

    Added Volume Slider
     - Added volume slider that keeps the value between scenes and when game is quit and opened again

---
+++ Worms Game/Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AudioSettings : MonoBehaviour
+{
+    float music;
+    public AudioSource backgroundMusic;
+    private static readonly string MusicPref = "MusicPref";
+
+    void Start()
+    {
+        ContinueSettings();
+    }
+
+    private void ContinueSettings()
+    {
+        music = PlayerPrefs.GetFloat(MusicPref);
+        Debug.Log(music);
+        backgroundMusic.volume = music;
+    }
+}
---
+++ Worms Game/Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeManager : MonoBehaviour
+{
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string MusicPref = "MusicPref";
+    public Slider volumeSlider;
+    float music;
+    int first;
+    public AudioSource backgroundMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        first = PlayerPrefs.GetInt(FirstPlay);
+
+        if (first == 0)
+        {
+            music = 0.25f;
+            volumeSlider.value = music;
+            PlayerPrefs.SetFloat(MusicPref, music);
+            PlayerPrefs.SetInt(FirstPlay, -1);
+        }
+        else
+        {
+            music = PlayerPrefs.GetFloat(MusicPref);
+            volumeSlider.value = music * 100;
+            Debug.Log(music);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SaveSound() 
+    {
+        PlayerPrefs.SetFloat(MusicPref, ((float)volumeSlider.value / 100));
+    }
+
+    public void OnApplicationFocus(bool inFocus)
+    {
+        if(!inFocus) 
+        {
+            SaveSound();
+        }
+    }
+
+    public void UpdateSound()
+    {
+        backgroundMusic.volume = ((float)volumeSlider.value / 100);
+    }
+}
